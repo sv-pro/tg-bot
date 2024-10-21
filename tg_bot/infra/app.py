82c59a02970106a5a771cce60b0554b58f30324e
sv-pro <sergeyvlasov.pro@gmail.com>
2024-10-21 09:26:43 +0300

    bot framework in progress

--- tg_bot/infra/app.py
+++ tg_bot/infra/app.py
@@ -25,12 +25,23 @@ BOT_TOKEN = settings.BOT_TOKEN
 
 def async_callable_wrapper(callback):
     async def async_callable(update: Update, *args, **kwargs):
-        message = update.message.text
-        response = callback(message)
+
+        kwargs["message"] = update.message.text
+        kwargs["command"] = update.message.text.split()[0]
+        kwargs["update"] = update
+        kwargs["context"] = args[0]
+
+        kwargs = {k: v for k, v in kwargs.items() if k in set(callback.__code__.co_varnames)}
+
+        # pass the rest of the arguments, but only if they are supported by the callback
+        response = callback(**kwargs)
         await update.message.reply_text(response)
     return async_callable
 
 class App:
+
+    default_message_handler = None
+    # default_command_handler = None
 
     def __init__(self,
                  start_callback: callable =None,
@@ -52,16 +63,22 @@ class App:
         self.application.add_handler(CommandHandler(trigger, async_callable_wrapper(callback)))
 
     def run(self):
+        # if self.default_command_handler:
+        #     self.application.add_handler(MessageHandler(FILTER_CATCH_ALL_COMMANDS, self.default_command_handler))
+
+        if self.default_message_handler:
+            self.application.add_handler(MessageHandler(FILTER_CATCH_ALL_MESSAGES, self.default_message_handler))
+
         self.application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-    def set_default_message_handler(self, default_message_callback):
+    def set_default_message_handler(self, default_message_callback: callable):
         """
         Add a default handler for all messages that do not match any of the registered handlers
         """
-        self.application.add_handler(MessageHandler(FILTER_CATCH_ALL_MESSAGES, async_callable_wrapper(default_message_callback)))
+        self.default_message_handler = async_callable_wrapper(default_message_callback)
 
-    def set_default_command_handler(self, default_command_callback):
-        """
-        Add a default handler for all commands that do not match any of the registered handlers
-        """
-        self.application.add_handler(MessageHandler(FILTER_CATCH_ALL_COMMANDS, async_callable_wrapper(default_command_callback)))
+    # def set_default_command_handler(self, default_command_callback: callable):
+    #     """
+    #     Add a default handler for all commands that do not match any of the registered handlers
+    #     """
+    #     self.default_command_handler = async_callable_wrapper(default_command_callback)
--- tg_bot/infra/examples/echo_start_bot.py
+++ tg_bot/infra/examples/echo_start_bot.py
@@ -5,7 +5,6 @@ from tg_bot.infra.app import App
 
 #####################################################################################
 if __name__ == "__main__":
-    print("Running the base module")
     # application code
     ## the application is responsible for handling the business logic
     ## each method in the application code is a handler for a specific command
@@ -15,12 +14,21 @@ if __name__ == "__main__":
     def start_callback(message: str) -> str:
         return "Hello, I am an echo bot. I will repeat whatever you say"
 
-    def default_callback(message: str) -> str:
+    def default_msg_callback(message: str) -> str:
+        return message
+
+    def default_cmd_callback(cmd: str) -> str:
+        return cmd
+
+    def delayed_callback(message: str, *args, **kwargs) -> str:
+        import time
+        time.sleep(5)
         return message
 
     app = App(start_callback=start_callback)
-    app.set_default_command_handler(default_callback)
-    app.set_default_message_handler(default_callback)
+    # app.set_default_command_handler(default_cmd_callback)
+    app.set_default_message_handler(default_msg_callback)
+    app.add_command_handler("delay", delayed_callback)
 
     ## start the app
     app.run()
